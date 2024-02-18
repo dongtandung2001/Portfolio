8bbc7546b3a20f97b5dd2d8f16e852ba82251ba7
Dung Dong <dongtandung2001@gmail.com>
2024-02-17 16:48:32 -0800

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,11 @@ import sql4u from "../assets/portfolio/sql4u.png";
 import memory from "../assets/portfolio/memory.png";
 import api from "../assets/portfolio/api.png";
 import storeapi from "../assets/portfolio/storeapi.png";
-import movierental from "../assets/portfolio/movierental.png";
 import discord from "../assets/portfolio/discord.png";
-import discordGpt from "../assets/portfolio/gpt.png"
+import discordGpt from "../assets/portfolio/gpt.png";
+
+import { Link } from "react-scroll";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Project = () => {
   const projects = [
@@ -28,7 +30,7 @@ const Project = () => {
       src: discordGpt,
       code: "https://github.com/dongtandung2001/aio-discord-bot",
       demo: "https://github.com/dongtandung2001/aio-discord-bot/",
-      title: "Discord.py, OpenAI API - Multifunctional Discord Bot",
+      title: "Discord.py - Multifunctional Discord Bot",
     },
     {
       id: 4,
@@ -62,13 +64,22 @@ const Project = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
-          <p className="py-6">Check some of my work here</p>
+          <p className="py-6">
+            Check some of my work here. More on my
+            <a href="https://github.com/dongtandung2001">
+              <button className="group text-white w-fit px-3 py-3 mx-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+                Github
+              </button>
+            </a>
+          </p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {projects.map(({ id, src, code, demo, title }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <h1 className="flex items-center justify-center text-center">{title}</h1>
+              <h1 className="flex items-center justify-center text-center">
+                {title}
+              </h1>
               <img
                 src={src}
                 alt=""
@@ -77,12 +88,16 @@ const Project = () => {
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   <a href={demo} target="_blank">
-                    Demo
+                    <button className="group text-white w-fit px-3 py-3 mx-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+                      Demo
+                    </button>
                   </a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   <a href={code} target="_blank">
-                    Code
+                    <button className="group text-white w-fit px-3 py-3 mx-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+                      Code
+                    </button>
                   </a>
                 </button>
               </div>
